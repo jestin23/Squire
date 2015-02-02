0dbe126d8874d0940f96a9b294dbdad175de5d74
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-02-02 11:00:53 +0000

    iCloud Drive linked with GitHub
    If I alter files in iCloud Drive, they are synced to GitHub.
    
    If I alter files in GitHub, they are synced to iCloud Drive.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Squire
+Squire - INSE Project Planning Web App [Group Coursework]
+
+When updating the source, please notify all users through official chat
+
+When updating keep pushing your altered source with commit messages & with good quality commenting
+
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php
+			$title = "Squire - University of Portsmouth";
+			include( "includes.inse.maxfalstein.co.uk/com/head.php" );
+		?>
+	</head>
+	<body>
+		<?php
+			include( "includes.inse.maxfalstein.co.uk/com/welcome.php" );
+			include( "includes.inse.maxfalstein.co.uk/index/information.php" );
+			include( "includes.inse.maxfalstein.co.uk/index/chart_links.php" );
+			include( "includes.inse.maxfalstein.co.uk/com/" );
+			include( "includes.inse.maxfalstein.co.uk/com/" );
+			include( "includes.inse.maxfalstein.co.uk/com/web_admin_contact.php" );
+			include( "includes.inse.maxfalstein.co.uk/com/cookies.php" );
+			include( "includes.inse.maxfalstein.co.uk/com/footer.php" );
+			// Footer will sort out the footer cookie
+		?>
