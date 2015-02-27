d8ed4fb8466f0508f296f45f3128a3e1b0b8dd24
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-02-27 11:54:30 +0000

    Create cookies.php
    Create cookies.php include file to include cookies in com directory.

---
+++ com/cookies.php
@@ -0,0 +1,7 @@
+<h1>Hello</h1>
+<?php
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
+?>
