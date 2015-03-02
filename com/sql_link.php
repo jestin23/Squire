0c057478db5591cb606eb3e3f5c6b6ac527faa30
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-03-02 15:59:03 +0000

    Fixed formatting issues with file.

--- com/sql_link.php
+++ com/sql_link.php
@@ -1,25 +1,18 @@
 // SQL linking code
-
-  <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "root";
-  $dbname = "Squire_UoP"; //setting variables
-
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); //connects to the database with the name and password set in the variables
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-
-    // SQL statment using the PHP of the forms to go here.
-    // Should we run inclusion files for different SQL queries?
-
-    // Return the results here in JSON.
-    }
-
-      catch(PDOException $e)
-      {
-        echo $sql . "<br>" . $e->getMessage();
-      } // If not database is found and exception is thrown. 
-
-      $conn = null; //sets connection to null and ends the connection
-      ?>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "root";
+                $dbname = "Squire_UoP"; //setting variables
+                try {
+                    // Connects to the database with the name and password set in the appropriate variables
+                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    // SQL statment using the PHP of the forms to go here.
+                    // Should we run inclusion files for different SQL queries?
+                    // Return the results here in JSON.
+                } catch(PDOException $e) {
+                        echo $sql . "<br>" . $e->getMessage();
+                    } // If not database is found and exception is thrown. 
+                $conn = null; //sets connection to null and ends the connection
+            ?>
