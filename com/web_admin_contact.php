5140aa12c4f78eb6cd729dbbab313824ec49bd58
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-03-02 15:43:43 +0000

    Added all details
    I need to add the surnames of all users, but the format is complete and populated with test data.
    
    Handles have been currently omitted.

--- com/web_admin_contact.php
+++ com/web_admin_contact.php
@@ -1,1 +1,59 @@
 <h1>Hello</h1>
+
+
+// html version
+
+        <section id="team_members">
+            <tr>
+                <td>Dan</td>
+                <td>Request Handle</td>
+            </tr>
+            <tr>
+                <td>Jestin</td>
+                <td>Request Handle</td>
+            </tr>
+            <tr>
+                <td>MaX Falstein</td>
+                <td>Request Handle</td>
+            </tr>
+            <tr>
+                <td>Rafael</td>
+                <td>Request Handle</td>
+            </tr>
+            <tr>
+                <td>Roja</td>
+                <td>Request Handle</td>
+            </tr>
+            <tr>
+                <td>Tom Phillippe</td>
+                <td>Request Handle</td>
+            </tr>
+        </section>
+        <address id="address">
+            <p>MaX Falstein</p>
+            <ul>
+                <li>University of Portsmouth</li>
+                <li>Big Data Lab 0.14</li>
+                <li>Buckingham Building</li>
+                <li>Lion Gate Terrace</li>
+                <li>Portsmouth</li>
+                <li>Hampshire</li>
+                <li>PO1 3HE</li>
+                <li>United Kingdom</li>
+            </ul>
+        <address>
+
+
+
+
+
+
+// php version
+
+<?php
+    echo "";
+    echo "";
+    echo "";
+    echo "";
+    echo "";
+?>
