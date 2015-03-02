1fab47fdbb002a378f8cd1927cce595b8a721daf
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-03-02 15:48:29 +0000

    Added file names in directories

--- com/cookies.php
+++ com/cookies.php
@@ -1,7 +1,12 @@
 <h1>Hello</h1>
 <?php
-    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
-    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
-    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
-    include( "includes.squire.inse.maxfalstein.co.uk/cookies/" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user.html" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user.js" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user.php" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user1.html" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user1.js" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user1.php" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user2.html" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user2.js" );
+    include( "includes.squire.inse.maxfalstein.co.uk/cookies/user2.php" );
 ?>
