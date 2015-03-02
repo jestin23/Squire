dd972c9cc9370c81cd175c008f168cd86d88f738
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-03-02 16:11:41 +0000

    Update index.php

--- settings/index.php
+++ settings/index.php
@@ -11,11 +11,12 @@
 	<body>
 		<?php
 			include( "inse.maxfalstein.co.uk/includes/com/welcome.php" ); // I am not sure on this file name
-			include( "inse.maxfalstein.co.uk/includes/index/" );
-			include( "inse.maxfalstein.co.uk/includes/index/" );
-			include( "inse.maxfalstein.co.uk/includes/index/" );
-			include( "inse.maxfalstein.co.uk/includes/index/" );
-			include( "inse.maxfalstein.co.uk/includes/index/" );
+			include( "inse.maxfalstein.co.uk/includes/com/" );
+			include( "inse.maxfalstein.co.uk/includes/settings/" );
+			include( "inse.maxfalstein.co.uk/includes/settings/" );
+			include( "inse.maxfalstein.co.uk/includes/settings/" );
+			include( "inse.maxfalstein.co.uk/includes/settings/" ); // Cookie update code is within this file
+			include( "inse.maxfalstein.co.uk/includes/cookies/cookies.php" );
 			include( "inse.maxfalstein.co.uk/includes/com/web_admin_contact.php" );
 			include( "inse.maxfalstein.co.uk/includes/com/footer.php" );
 		?>
