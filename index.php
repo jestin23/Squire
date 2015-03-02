8129518541e04ddf2668d1c5dad708c7601a03a9
TomPhillippe <tomhphillippe@aol.com>
2015-03-02 14:42:48 +0000

    Update index.php
    I have added the code that should connect us to the database, we will need to work out how we can all access the database as i don't know how to do this.
    MaX will probably take this code and put into a file to make the page look neater, well thats what i would do anyway but obviously i don't have access to max's files. 
    The comments should help you understand what the code is doing :)

--- index.php
+++ index.php
@@ -18,3 +18,26 @@
 			include( "includes.inse.maxfalstein.co.uk/com/footer.php" );
 			// Footer will sort out the footer cookie
 		?>
+		
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "Squire_UoP"; //setting variables
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password); //connects to the database with the name and password set in the variables
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+
+    // SQL statment using the PHP of the forms to go here... 
+
+    // Return the results here in JSON.
+    }
+
+      catch(PDOException $e)
+      {
+        echo $sql . "<br>" . $e->getMessage();
+      } // If not database is found and exception is thrown. 
+
+      $conn = null; //sets connection to null and ends the connection
+      ?>
