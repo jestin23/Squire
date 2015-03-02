7c1b0bdc9e1957bbea1bf8aacc18a432f82782d6
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-03-02 16:16:35 +0000

    Update index.php

---
+++ tools/index.php
@@ -0,0 +1,24 @@
+<!-- Tools web page -->
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php
+			$title = "Squire Tools - University of Portsmouth";
+			include( "includes.inse.maxfalstein.co.uk/com/head.php" );
+		?>
+	</head>
+	<body>
+		<?php
+			include( "includes.inse.maxfalstein.co.uk/com/welcome.php" ); // I am not sure if this is required
+			include( "includes.inse.maxfalstein.co.uk/com/user_identifier.php" );
+			include( "includes.inse.maxfalstein.co.uk/tools/" );
+			include( "includes.inse.maxfalstein.co.uk/tools/" );
+			include( "includes.inse.maxfalstein.co.uk/tools/" );
+			include( "includes.inse.maxfalstein.co.uk/tools/" ); // Cookie update code is within this file
+			include( "includes.inse.maxfalstein.co.uk/cookies/cookies.php" );
+			include( "includes.inse.maxfalstein.co.uk/com/web_admin_contact.php" );
+			include( "includes.inse.maxfalstein.co.uk/com/footer.php" );
+		?>
+	</body>
+</html>
